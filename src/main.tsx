69d29fd95aa491509a221c850e963bb72a507fd3
FadilRifaldy <fadilrifaldy228@gmail.com>
2026-04-21 19:19:24 +0700

    initial setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,24 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+/* Ionic CSS */
+import '@ionic/react/css/core.css';
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme */
+import './theme/variables.css';
+
+/* Tailwind */
+import './index.css';
+
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
